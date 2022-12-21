5e28ac1f916e9ba19f315e2894b71908d52fde5c
Drew Pomatti <ampomatti@gmail.com>
2022-12-21 12:45:34 -0800

    started to set up routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import router from './router';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors()) //'cross origin research sharing'
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use((req, res, next) => {
+    res.status(401)
+    res.send('Nope')
+})
 
 app.get('/', (req, res) => {
     console.log('hello from express')
     res.status(200)
     res.json({message: 'hello'})
 })
+
+app.use('/api', router)
 
 export default app;

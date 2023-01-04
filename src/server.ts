c214e27ceb699b20f28c1b4f9980bae74f19bbd0
Drew Pomatti <ampomatti@gmail.com>
2023-01-03 16:50:02 -0800

    added JWTs and Auth

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import router from './router';
 import morgan from 'morgan';
 import cors from 'cors';
+import { protect } from './modules/auth'
+import { createNewUser } from './handlers/user';
+import { signin } from './handlers/user';
 
 const app = express();
 
@@ -10,17 +13,14 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.use((req, res, next) => {
-    res.status(401)
-    res.send('Nope')
-})
-
 app.get('/', (req, res) => {
     console.log('hello from express')
     res.status(200)
     res.json({message: 'hello'})
 })
 
-app.use('/api', router)
+app.use('/api', protect, router)
+app.post('/user', createNewUser) 
+app.post('/signin', signin)
 
 export default app;

d5fea6575db5a7c6bb03a5f281cb1116915628f4
Drew Pomatti <ampomatti@gmail.com>
2023-01-04 17:50:46 -0800

    added product route handlers

---
+++ src/handlers/product.ts
@@ -0,0 +1,71 @@
+import prisma from '../db'
+
+
+
+//get all products for user, using user table to access array of their products
+export const getUserProducts = async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            id: req.user.id
+        },
+        include: {
+            products: true
+        }
+    })
+
+    res.json({data: user.products})
+}
+
+//get one product for user
+export const getOneProduct = async (req, res) => {
+    const id = req.params.id
+
+    const product = await prisma.product.findFirst({
+        where: {
+            id,
+            belongsToId: req.user.id
+        }
+    })
+
+    res.json({data: product})
+}
+
+export const createProduct = async (req, res) => {
+    const product = await prisma.product.create({
+        data: {
+            name: req.body.name,
+            belongsToId: req.user.id
+        }
+    })
+
+    res.json({data: product})
+}
+
+export const updateProduct = async (req, res) => {
+    const updated = await prisma.product.update({
+        where: {
+            id_belongsToId: {
+                id: req.params.id,
+                belongsToId: req.user.id
+            }
+        },
+        data: {
+            name: req.body.name
+        }
+    })
+
+    res.json({data: updated})
+}
+
+export const deleteProduct = async (req, res) => {
+    const deleted = await prisma.product.delete({
+        where: {
+            id_belongsToId: {
+                id: req.params.id,
+                belongsToId: req.user.id
+            }
+        }
+    })
+
+    res.json({data: deleted})
+}
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 
@@ -8,22 +10,43 @@ router.get('/product', (req, res) => {
     res.json({message: 'hi there!'})
  })
 router.get('/product/:id', () => { })
-router.put('/product/:id', () => { })
-router.post('/product', () => { })
+router.put('/product/:id', body('name').isString(), handleInputErrors, (req, res) => {
+})
+
+router.post('/product', body('name').isString(), handleInputErrors, (req, res) => { })
 router.delete('/product/:id', () => { })
 
 //Update
 router.get('/update', () => { })
 router.get('/update/:id', () => { })
-router.put('/update/:id', () => { })
-router.post('/update', () => { })
+router.put('/update/:id',
+    body('title').optional(),
+    body('body').optional(),
+    body('status').isIn(['IN_PROGRESS', 'SHIPPED' ,'DEPRECATED']),
+    body('version').optional(),
+    () => { }
+)
+router.post('/update',
+    body('title').exists().isString,
+    body('body').exists().isString(),
+    () => { }
+)
 router.delete('/update/:id', () => { })
 
 //Update Point
 router.get('/updatepoint', () => { })
 router.get('/updatepoint/:id', () => { })
-router.put('/updatepoint/:id', () => { })
-router.post('/updatepont', () => { })
+router.put('/updatepoint/:id',
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+    () => { }
+)
+router.post('/updatepont',
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+    body('updateId').exists().isString(),
+    () => { }
+)
 router.delete('/updatepont/:id', () => { })
 
 export default router;

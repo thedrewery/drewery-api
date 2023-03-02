f85fa36becb6f0b9a4d3a0f156e1279a0c611277
Drew Pomatti <ampomatti@gmail.com>
2023-03-01 21:45:22 -0800

    readMe

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # drewery-api
+
+App Progress Management API
+
+API built for an app that allows Developers and Project Managers to track the progress of updates and new features in their products.
--- src/handlers/updatepoint.ts
+++ src/handlers/updatepoint.ts
@@ -27,6 +27,7 @@ export const getUserUpdatePoints = async (req, res) => {
     res.json({data: updates})
 }
 
+//creating an update point from a single update from a single product from a user
 export const createUpdatePoints = async (req, res) => {
     const product = await prisma.product.findUnique({
         where: {
@@ -52,49 +53,4 @@ export const createUpdatePoints = async (req, res) => {
         }
     })
     res.json({data: updatePoint})
-}
-
-export const updateUpdatePoint = async (req, res) => {
-    const products = await prisma.product.findMany({
-        where: {
-            belongsToId: req.user.id,
-        },
-        include: {
-            updates: true
-        }
-    })
-
-    const updates = products.reduce((allUpdates, product) => {
-        return [...allUpdates, ...product.updates]
-    }, [])
-
-    const match = updates.find(update => update.id === req.params.id)
-
-    if (!match) {
-        res.json({message: "No Match"})
-    }
-
-    const updatedUpdate = await prisma.update.update({
-        where: {
-            id: req.params.id
-        },
-        data: req.body
-    })
-
-    res.json({data: updatedUpdate})
-} 
-
-export const deleteUpdatePoint = async (req, res) => {
-    const products = await prisma.product.findMany({
-        where: {
-            belongsToId: req.user.id,
-        },
-        include: {
-            updates: true
-        }
-    })
-
-    const updates = products.reduce((allUpdates, product) => {
-        return [...allUpdates, ...product.updates]
-    }, [])
 }
